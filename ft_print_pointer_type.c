050e425ceb3421ecacf7f8cab096e30f65f1a262
Kllaster <noityt@mail.ru>
2021-01-18 20:22:32 +0300

    Fix hex type (%x/%X) & pointer type (%p)

--- ft_print_pointer_type.c
+++ ft_print_pointer_type.c
@@ -5,31 +5,25 @@ void	ft_print_pointer_width(t_arg_param param, int *print_c)
 	int start_chars;
 
 	start_chars = 0;
-	if (((param.precision - *print_c) > 0 ||
-		(FLG_MINUS & param.flag) || (param.width - (*print_c) - 2) < 0)
-		&& (start_chars = 1))
-		write(1, "0x", 2);
-	while ((param.precision - *print_c) > 0)
-	{
+	while ((param.precision - *print_c) > 0 && ++(*print_c))
 		write(1, "0", 1);
-		(*print_c)++;
-	}
-	if (!(FLG_MINUS & param.flag) && (param.width - (*print_c)) > 0)
+	if (!(FLG_MINUS & param.flag) && (param.width - *print_c) > 0)
 	{
-		if (*print_c == 1 && param.precision == 0)
-			*print_c = 0;
-		while ((param.width - (*print_c) - 2) > 0)
+		if ((param.precision - *print_c) > 0)
 		{
-			write(1, " ", 1);
-			(*print_c)++;
+			param.width -= param.precision - *print_c;
+			param.precision += param.width - *print_c;
 		}
-		if (start_chars == 0)
+		while ((param.width - *print_c) > 0 && ++(*print_c))
+			write(1, " ", 1);
+		if (start_chars == 0 && (start_chars = 1))
 			write(1, "0x", 2);
 	}
-	(*print_c) += 2;
+	if (start_chars == 0)
+		write(1, "0x", 2);
 }
 
-void	ft_print_pointer(unsigned long num, t_arg_param param, int *print_c)
+void	ft_print_pointer(unsigned long int num, t_arg_param param, int *print_c)
 {
 	int		hex;
 	char	c;
@@ -45,10 +39,7 @@ void	ft_print_pointer(unsigned long num, t_arg_param param, int *print_c)
 		ft_print_pointer(num, param, print_c);
 	else
 		ft_print_pointer_width(param, print_c);
-	if (!(param.precision == 0 && c == '0'))
-		write(1, &c, 1);
-	else
-		(*print_c)--;
+	write(1, &c, 1);
 }
 
 int		ft_print_pointer_two(t_arg_param param)
@@ -64,25 +55,31 @@ int		ft_print_pointer_two(t_arg_param param)
 int		ft_print_pointer_type(t_arg_param param, va_list *arg_ptr)
 {
 	int				print_c;
-	unsigned long	num;
+	unsigned long int	num;
 
-	num = (unsigned long)va_arg(*arg_ptr, void*);
-	print_c = 0;
-	if (param.precision > param.width)
+	num = (unsigned long int)va_arg(*arg_ptr, void*);
+	print_c = 2;
+	if (param.precision >= param.width)
 		param.width = 0;
-	if (param.width == 2 && !num)
+	if (!num && param.width == 2)
 		return (ft_print_pointer_two(param));
+	if (num == 0 && param.precision == 0)
+	{
+		if ((FLG_MINUS & param.flag))
+			write(1, "0x", 2);
+		ft_print_repeat_char(' ', param.width - 2);
+		if (!(FLG_MINUS & param.flag))
+			write(1, "0x", 2);
+		if ((param.width - 2) < 0)
+			param.width = 2;
+		return ((param.width - 2) + 2);
+	}
 	ft_print_pointer(num, param, &print_c);
 	if (!(FLG_MINUS & param.flag) && print_c == param.width - 1
-			&& param.precision == 0)
+		&& param.precision == 0)
 		print_c = param.width;
 	if (FLG_MINUS & param.flag)
-	{
-		while ((param.width - print_c) > 0)
-		{
+		while ((param.width - print_c) > 0 && ++print_c)
 			write(1, " ", 1);
-			print_c++;
-		}
-	}
 	return (print_c);
 }
